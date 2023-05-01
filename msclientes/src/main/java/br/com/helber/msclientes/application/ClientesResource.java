a52c2aafd67e715dbdc8669940e880eab9a85dbf
Helber Luz <125887562+LuzHelber@users.noreply.github.com>
2023-05-01 18:11:06 -0300

    Add files via upload
    integração com o intellij

---
+++ msclientes/src/main/java/br/com/helber/msclientes/application/ClientesResource.java
@@ -0,0 +1,46 @@
+package br.com.helber.msclientes.application;
+
+import br.com.helber.msclientes.application.representation.ClienteSaveRequest;
+import br.com.helber.msclientes.domain.Cliente;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("clientes")
+@RequiredArgsConstructor
+@Slf4j
+public class ClientesResource {
+
+    private final ClienteService service;
+    @GetMapping
+    public String status() {
+        log.info("Obtendo o status do microservice de clientes");
+        return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody ClienteSaveRequest request) {
+       var cliente = request.toModel();
+       service.save(cliente);
+        URI hearderLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(cliente.getCpf())
+                .toUri();
+        return ResponseEntity.created(hearderLocation).build();
+
+    }
+    @GetMapping(params = "cpf")
+    public ResponseEntity dadosCliente(@RequestParam("cpf") String cpf){
+        var cliente = service.getByCPF(cpf);
+        if(cliente.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente);
+    }
+}
